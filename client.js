ef39d698920d8bf45c4cd21767467e427e3d2878
muitagathondu <91227327+muitagathondu@users.noreply.github.com>
2025-02-09 17:53:14 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,15 @@
+const peer = new Peer('my-client-id', {
+    host: 'https://myotherpeerjs.onrender.com', // Replace with your Render service name
+    port: 443,        // Use 443 for HTTPS
+    path: '/myapp',
+    secure: true      // Ensure the connection is secure
+  });
+  
+  peer.on('open', (id) => {
+    console.log('Connected to PeerJS server with ID:', id);
+  });
+  
+  peer.on('error', (err) => {
+    console.error('Error:', err);
+  });
+  
