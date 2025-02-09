10a0a4bf5a4e9d8fb9bce5792e174b9de9d88041
muitagathondu <91227327+muitagathondu@users.noreply.github.com>
2025-02-09 18:23:13 +0300

    Add files via upload

--- client.js
+++ client.js
@@ -1,15 +1,50 @@
-const peer = new Peer('1', {
-    host: 'https://myotherpeerjs.onrender.com', // Replace with your Render service name
-    port: 443,        // Use 443 for HTTPS
-    path: '/myapp',
-    secure: true      // Ensure the connection is secure
-  });
-  
-  peer.on('open', (id) => {
-    console.log('Connected to PeerJS server with ID:', id);
-  });
-  
-  peer.on('error', (err) => {
-    console.error('Error:', err);
-  });
-  
+
+
+  // Global peer variable
+let peer;
+
+// Updated `init` function with only `userId` as a parameter
+window.init = async function init(userId) {
+    try {
+       // const ipAddress = await fetchIpFromFirestore(); // Fetch IP address
+       // console.log("Initializing Peer with IP:", ipAddress);
+
+        peer = new Peer(userId, {
+        
+        host:'mypeerjswebsite.onrender.com',
+
+         // host:'192.168.100.6',
+
+         // host:'localhost',
+
+        
+
+     
+          
+         port: 443, // HTTPS default port
+
+         // port: 9000, // HTTPS default port
+      
+
+          // path: '/peerjs',
+
+           path: '/myapp',
+
+          
+       
+          secure: true ,// Use secure connections,
+
+        
+
+        });
+
+        peer.on('open', () => {
+          //  Android.onPeerConnected(); // Ensure this is being called
+            console.log("Peer connected successfully.");
+        });
+
+        listen(); // Start listening for calls
+    } catch (error) {
+        console.error("Error initializing Peer:", error);
+    }
+}
